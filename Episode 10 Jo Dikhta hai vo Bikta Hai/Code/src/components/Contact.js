14151ebaca41cf573cc892549a3177acfbedb257
Sambhav <sambhav12wakhariya@gmail.com>
2025-03-15 21:30:08 +0530

    Checked In : Episode 10 Components Folder

---
+++ Episode 10 Jo Dikhta hai vo Bikta Hai/Code/src/components/Contact.js
@@ -0,0 +1,9 @@
+const Contact = ()=>{
+    return(
+        <div className="p-5">
+            <h1>Contact Us Page</h1>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ Episode 10 Jo Dikhta hai vo Bikta Hai/Code/src/components/Header.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import useOnlineStatus from '../utils/useOnlineStatus';
+
+const Header = () => {
+
+  let [btnName , setBtnName] = useState('Login');
+  // Creating a state variable
+
+  const onlineStatus = useOnlineStatus();
+
+  useEffect(()=>{
+    console.log("Use Effect Called");
+  }, [btnName])
+
+  console.log("Header Rendered");
+
+  return (
+    <div className="header p-3 m-5">
+      <div className="logo">
+        <img
+          className="logo"
+          src="https://img.freepik.com/premium-vector/food-ordering-app-logo-with-points-fork-shapes-center_666184-195.jpg"
+          alt=""
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+        <li>Online Status: {onlineStatus ? '✅' : '⛔'}</li>
+          <li>
+          <Link to='/' >Home</Link>
+          </li>
+          <li>
+          <Link to='/about' >About Us</Link>
+          </li>
+          <li>
+            <Link to='/contact' >Contact Us</Link>
+          </li>
+          <li>
+            <Link to='/grocery' >Grocery</Link>
+          </li>
+          <li>Cart</li>
+          <button className='login m-3 py-1 px-3 rounded-md bg-gray-500 text-white' onClick={()=> {
+            btnName === "Login" ? setBtnName('Logout') : setBtnName('Login');
+          } }>{btnName}</button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
