70169c0eebd9afe480b020ebbe518a1b0491707e
Sambhav <sambhav12wakhariya@gmail.com>
2025-03-19 23:58:52 +0530

    Checked In : Episode 11 Utils

---
+++ Episode 11 Data is the New Oil/Code/src/utils/constants.js
@@ -0,0 +1,3 @@
+export const IMAGE_URL = 'https://media-assets.swiggy.com/'
+
+export const REST_MENU = 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.1933463&lng=77.4113845&restaurantId='
